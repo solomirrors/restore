6eae78577907d4809b522bb438be4ce415c2a509
solomirrors <solomirrors@icloud.com>
2021-11-21 02:52:27 +0100

    design prototype

--- src/components/pages/main-pages/main-pages.js
+++ src/components/pages/main-pages/main-pages.js
@@ -1,6 +1,18 @@
 import React, {Component} from "react";
 import './main-pages.css'
-import {CardImg, Col, Container, Image, Nav, Navbar, NavbarBrand, NavDropdown, NavLink, Row} from "react-bootstrap";
+import {
+    Button,
+    CardImg,
+    Col,
+    Container,
+    Image,
+    Nav,
+    Navbar,
+    NavbarBrand,
+    NavDropdown,
+    NavLink,
+    Row
+} from "react-bootstrap";
 import Spinner from "../../spinner";
 import CardHeader from "react-bootstrap/CardHeader";
 import {motion} from "framer-motion/dist/framer-motion"
@@ -9,7 +21,7 @@ export default class MainPages extends Component{
     render() {
         return(
             <React.Fragment>
-                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                <Navbar collapseOnSelect expand="sm" className='re-header-bar'>
                     <Container fluid className='p-1 m-0'>
                         <Navbar.Brand href="#home">
                             <h3 className='re-store-name d-flex align-items-center'>
@@ -29,146 +41,52 @@ export default class MainPages extends Component{
                             </h3>
                         </Navbar.Brand>
                         <Navbar.Toggle aria-controls="responsive-navbar-nav ju" />
-                        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-between p-3 align-items-center'>
-                            <Nav className="me-auto">
-                                <NavDropdown title="Books" id="collasible-nav-dropdown" style={{fontSize: '26px'}}>
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                                <NavDropdown title="Fiction" id="collasible-nav-dropdown" style={{fontSize: '26px'}}>
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                                <NavDropdown title="Nonfiction" id="collasible-nav-dropdown" style={{fontSize: '26px'}}>
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                                <NavDropdown title="eBooks" id="collasible-nav-dropdown" style={{fontSize: '26px'}}>
-                                    <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
-                                    <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
-                                    <NavDropdown.Divider />
-                                    <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-                                </NavDropdown>
-                            </Nav>
+                        <Navbar.Collapse id="responsive-navbar-nav" className='justify-content-end p-3 align-items-center'>
                             <Nav>
-                                <Nav.Link href="#deets" style={{fontSize: '26px'}}>More deets</Nav.Link>
-                                <Nav.Link eventKey={2} href="#memes" style={{fontSize: '26px'}}>
-                                    Dank memes
-                                </Nav.Link>
+                                <NavLink><Button>Registration</Button></NavLink>
+                                <NavLink><Button>Login</Button></NavLink>
+                                <NavLink><Button>Cart</Button></NavLink>
                             </Nav>
                         </Navbar.Collapse>
                     </Container>
                 </Navbar>
                 <div className='re-store-splint'/>
-                <Container fluid className='re-store-header m-0 p-4'>
-                    <Row className='m-0'>
-                        <Col className='d-flex justify-content-center align-items-center'>
-                            <div className='re-logotype material-icons'>
-                                bookmarks
-                                <h4 className='m-3'>BESTSELLERS</h4>
-                            </div>
-                        </Col>
-                        <Col className='d-flex justify-content-center align-items-center'>
-                            <div className='re-logotype material-icons'>
-                                bookmarks
-                                <h4 className='m-3'>NEW RELEASES</h4>
-                            </div>
-                        </Col>
-
-                        <Col className='d-flex justify-content-center align-items-center'>
-                            <div className='re-logotype material-icons'>
-                                bookmarks
-                                <h4 className='m-3'>COMING SOON</h4>
-                            </div>
-                        </Col>
-                        <Col className='d-flex justify-content-center align-items-center'>
-                            <div className='re-logotype material-icons'>
-                                bookmarks
-                                <h4 className='m-3'>MANGA</h4>
-                            </div>
-                        </Col>
-                    </Row>
-                </Container>
+                <Navbar className='re-header-link' collapseOnSelect expand="md">
+                    <Container fluid className='m-1 p-0 d-flex align-items-center justify-content-center'>
+                        <Navbar.Toggle aria-controls="responsive-navbar-nav ju" style={{color: '#fff'}}/>
+                        <Navbar.Collapse id="basic-navbar-nav w-100">
+                        <Nav className="m-1 re-nav-dropdown w-100 justify-content-around align-items-center">
+                            <NavDropdown title="Books" id="re-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Item Divider</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Fiction" id="re-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Item Divider</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Nonfiction" id="re-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Item Divider</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="eBooks & Nook" id="re-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Divider />
+                                <NavDropdown.Item href="#action/3.4">Item Divider</NavDropdown.Item>
+                            </NavDropdown>
+                            <NavDropdown title="Teens & YA Books" id="re-nav-dropdown">
+                                <button className="dropdown-item" type="button">Action</button>
+                                <button className="dropdown-item" type="button">Another action</button>
+                                <button className="dropdown-item" type="button">Something else here</button>
+                            </NavDropdown>
+                        </Nav>
+                        </Navbar.Collapse>
+                    </Container>
+                </Navbar>
                 <div className='re-store-splint'/>
-                <Container fluid className='re-store-book m-0 p-4'>
-                    <Row className='re-store-bestseller d-flex align-self-baseline'>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                The 1619 Project
-                            </CardHeader>
-                            <motion.div
-                                style={{width: '100%', height: '100%'}}
-                                animate={{ rotate: 180 }}
-                                transition={{
-                                    repeat: 2,
-                                    repeatType: "reverse",
-                                    duration: 2
-                                }}
-                            >
-                                <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780593230572_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            </motion.div>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                The Real Anthology Fauci
-                            </CardHeader>
-                            <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9781510766808_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            <div className='test-text'>Des Boooks</div>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                Mercy
-                            </CardHeader>
-                            <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9781538706091_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                Will
-                            </CardHeader>
-                            <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9781984877925_p0_v3%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                Lincoln Highway
-                            </CardHeader>
-                            <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9780735222359_p0_v5%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                        <Col sm={4} md={3} xl={2} className='p-1'>
-                            <CardHeader>
-                                A Plague Upon Our House
-                            </CardHeader>
-                            <CardImg src='https://prodimage.images-bn.com/lf?set=key%5Bresolve.pixelRatio%5D,value%5B1%5D&set=key%5Bresolve.width%5D,value%5B550%5D&set=key%5Bresolve.height%5D,value%5B10000%5D&set=key%5Bresolve.imageFit%5D,value%5Bcontainerwidth%5D&set=key%5Bresolve.allowImageUpscaling%5D,value%5B0%5D&product=path%5B/pimages/9781637582206_p0_v1%5D&call=url%5Bfile:common/decodeProduct.chain%5D'/>
-                            <CardHeader>
-                                Des Books
-                            </CardHeader>
-                        </Col>
-                    </Row>
-                </Container>
+
             </React.Fragment>
 
         )
